ec6756aee5580f35c018dbdafc5c00e91d29196d
mustafasteadfast <mustafasteadfast@gmail.com>
2025-03-01 13:26:24 +0600

    student class

--- main.dart
+++ main.dart
@@ -1,36 +1,24 @@
-void main() {
-  Person person = Person();
-  Person person2 = Person();
-
-  // info for person
-  person.name = "John";
-  person.family = "Doe";
-  person.age = 30;
-  person.isMarried = true;
-
-  // info for person2
-  person2.name = "Jane";
-  person2.family = "Doe";
-  person2.age = 25;
-  person2.isMarried = false;
-
-  // display info for person instance
-  person.display();
-  // display info for person2 instance
-  person2.display();
-}
-
-class Person {
-  // String name = "John";
-  // String family = "Doe";
-  // int age = 30;
-
+class Student {
   String? name;
-  String? family;
   int? age;
-  bool? isMarried;
+  String? grade;
+  int? id;
 
   void display() {
-    print("My name is $name $family and I am $age years old. I am ${isMarried == true ? 'married' : 'not married'}.");
+    print("Name: $name");
+    print("Age: $age");
+    print("Grade: $grade");
+    print("ID: $id");
   }
+}
+
+void main() {
+  Student student = Student();
+
+  student.name = "John";
+  student.age = 20;
+  student.grade = "A";  
+  student.id = 1234;
+
+  student.display();
 }
